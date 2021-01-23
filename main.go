4984521099ed5a8b6d6a06fb6a5d5373e7d89ef0
Mitchell Lisle <lislemitchell@gmail.com>
2021-01-23 14:00:55 +0000

    add: departures from london

--- main.go
+++ main.go
@@ -10,6 +10,39 @@ import (
 	"net/http"
 )
 
+func getSydneyArrivals(avista *aviationStack.AviationStack, storage *gcp.GCS, folder string) {
+	var sydneyChannel = make(chan aviationStack.Flight)
+	var params = make(map[string]string)
+	params["arr_iata"] = "SYD"
+
+	go avista.StreamFlights(sydneyChannel, params)
+
+	payload := aviationStack.CollectRecords(sydneyChannel)
+
+	folder = fmt.Sprintf("%s/sydney", folder)
+	fileName := utils.GenerateFileName(folder)
+
+	log.Println(fmt.Sprintf("writing file %s", fileName))
+	storage.UploadFile(payload, fileName, "application/json")
+
+}
+
+func getLondonDepartures(avista *aviationStack.AviationStack, storage *gcp.GCS, folder string) {
+	var londonChannel = make(chan aviationStack.Flight)
+	var params = make(map[string]string)
+	params["dep_iata"] = "LHR"
+
+	go avista.StreamFlights(londonChannel, params)
+
+	payload := aviationStack.CollectRecords(londonChannel)
+
+	folder = fmt.Sprintf("%s/london", folder)
+	fileName := utils.GenerateFileName(folder)
+
+	log.Println(fmt.Sprintf("writing file %s", fileName))
+	storage.UploadFile(payload, fileName, "application/json")
+}
+
 func main() {
 	AppConfig := config.NewAppConfig()
 
@@ -20,15 +53,8 @@ func main() {
 
 	storage := gcp.InitGCS(AppConfig.GCPConfig.Project, AppConfig.GCPConfig.StorageBucket)
 
-	var flightChannel = make(chan aviationStack.Flight)
-	go avista.StreamFlights(flightChannel, AppConfig.AvistaConfig.ArrivalCode)
-
-	payload := aviationStack.CollectRecords(flightChannel)
-
-	fileName := utils.GenerateFileName(AppConfig.GCPConfig.StorageFolder)
-
-	log.Println(fmt.Sprintf("writing file %s", fileName))
-	storage.UploadFile(payload, fileName, "application/json")
+	getSydneyArrivals(avista, storage, AppConfig.GCPConfig.StorageFolder)
+	getLondonDepartures(avista, storage, AppConfig.GCPConfig.StorageFolder)
 
 	log.Println("finished")
 }
